e12bf383b686c16d0ec8be4e6dec622d381b7078
Helio Franca <heliordf@hotmail.com>
2024-06-18 23:33:28 -0300

    Update

--- src/main/java/org/example/Main3.java
+++ src/main/java/org/example/Main3.java
@@ -4,59 +4,48 @@ import java.text.DecimalFormat;
 
 public class Main3 {
     public static DecimalFormat df = new DecimalFormat("#,###");
-    public static DecimalFormat dfp = new DecimalFormat("##.#");
-    public static double deposito=125000;
-    public static double juros=30;
-    public static double meses=24;
-
-    public static double lucro=0;
-    public static double saldoFinal=0;
-
-
+    public static DecimalFormat dfp = new DecimalFormat("##.##");
+    public static double deposito =  55000;
+    public static double juros = 30;
+    public static double meses = 24;
+    public static double lucro = 0;
+    public static double adicional =2000;
 
 
     public static void main(String[] args) {
         info();
         calcularLucro();
     }
-
-    public static void info(){
-
+    public static void info() {
         System.out.println("========================================================");
-        System.out.println("Meses: "+df.format(meses));
-        System.out.println("Deposito: R$"+df.format(deposito));
-        System.out.println("Juros Ano: "+dfp.format(juros)+"%" + " | Juros Mês: "+dfp.format(juros/12)+"%");
+        System.out.println("Meses: " + df.format(meses));
+        System.out.println("Deposito: R$" + df.format(deposito));
+        System.out.println("Juros Ano: " + dfp.format(juros) + "%" + " | Juros Mês: " + dfp.format(juros / 12) + "%");
         System.out.println("========================================================");
     }
 
-    public static void calcularLucro(){
-        int cont=1;
-        double investimento=deposito;
-        double jurosMesPerc = juros/12;
+    public static void calcularLucro() {
+        int cont = 1;
+        double investimento = deposito;
+        double jurosMesPerc = juros / 12;
         double saldo = 0;
-        System.out.println("Ref | investimento |pagamento | lucroMes  | totalMes |lucro          | Saldo");
+        System.out.println("Ref | investimento | pagamento | lucroMes     | totalMes  | lucro            | Saldo");
 
-        while (cont<=meses){
-            double pagamento = deposito/meses;
-            double lucroMes= (investimento/100)*jurosMesPerc;
-            double totalMes= pagamento+lucroMes;
-            saldo+=totalMes;
-            investimento-=pagamento;
-            lucro+=lucroMes;
-            while(saldo>=1000){
-            saldo-=1000;
-            investimento+=1000;
+        while (cont <= meses) {
+            double pagamento = deposito / meses;
+            double lucroMes = (investimento / 100) * jurosMesPerc;
+            double totalMes = pagamento + lucroMes;
+            saldo += totalMes;
+            investimento -= pagamento;
+            lucro += lucroMes;
+            investimento += adicional;
+            while (saldo >= 1000) {
+                saldo -= 1000;
+                investimento += 1000;
+                deposito+= 1000;
             }
-            System.out.println(cont+"   | R$"+df.format(investimento)+ "     | R$"+df.format(pagamento)+"  | R$"+df.format(lucroMes)+"     | R$"+df.format(totalMes) +"  | R$"+ df.format(lucro)+" ("+dfp.format(lucro/investimento*100)+"%)  | R$"+ df.format(saldo));
+            System.out.println(cont + "   | R$" + df.format(investimento) + "     | R$" + df.format(pagamento) + "  | R$" + df.format(lucroMes) + "     | R$" + df.format(totalMes) + "  | R$" + df.format(lucro) + " (" + dfp.format(lucro / investimento * 100) + "%)  | R$" + df.format(saldo));
             cont++;
         }
-
     }
-
-
-
-
-
-
-
 }
